27ec01cc12fbaea472d12a820615ce44bb0ba57c
Grzegorz Hejduk <hejduk.grzegorz@gmail.com>
2016-01-23 18:59:37 +0100

    Update db schema file

--- src/main/resources/db/schema.sql
+++ src/main/resources/db/schema.sql
@@ -1,4 +1,27 @@
+-- MySQL dump 10.13  Distrib 5.6.27, for debian-linux-gnu (x86_64)
+--
+-- Host: 127.0.0.1    Database: bank
+-- ------------------------------------------------------
+-- Server version	5.6.26
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `account`
+--
+
 DROP TABLE IF EXISTS `account`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
 CREATE TABLE `account` (
   `number` char(6) NOT NULL,
   `balance` int(11) NOT NULL DEFAULT '0',
@@ -7,8 +30,39 @@ CREATE TABLE `account` (
   KEY `account_user_id_fk` (`owner_id`),
   CONSTRAINT `account_user_id_fk` FOREIGN KEY (`owner_id`) REFERENCES `user` (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Table structure for table `transaction`
+--
+
+DROP TABLE IF EXISTS `transaction`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `transaction` (
+  `id` char(36) NOT NULL,
+  `date` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `from` char(6) NOT NULL,
+  `to` char(6) NOT NULL,
+  `amount` double NOT NULL,
+  `status` int(11) NOT NULL,
+  PRIMARY KEY (`id`),
+  KEY `transaction_from_index` (`from`),
+  KEY `transaction_to_index` (`to`),
+  KEY `transaction_from_pk` (`from`),
+  CONSTRAINT `transaction_account_from_number_fk` FOREIGN KEY (`from`) REFERENCES `account` (`number`),
+  CONSTRAINT `transaction_account_number_fk` FOREIGN KEY (`from`) REFERENCES `account` (`number`),
+  CONSTRAINT `transaction_account_to_number_fk` FOREIGN KEY (`to`) REFERENCES `account` (`number`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Table structure for table `user`
+--
 
 DROP TABLE IF EXISTS `user`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
 CREATE TABLE `user` (
   `id` char(36) NOT NULL,
   `email` varchar(100) NOT NULL,
@@ -20,14 +74,15 @@ CREATE TABLE `user` (
   UNIQUE KEY `user_email_uindex` (`email`),
   KEY `user_email_index` (`email`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
 
-DROP TABLE IF EXISTS `transaction`;
-CREATE TABLE `transaction` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
-  `date` datetime NOT NULL,
-  `from` char(6) NOT NULL,
-  `to` char(6) NOT NULL,
-  `amount` int(11) NOT NULL,
-  PRIMARY KEY (`id`),
-  UNIQUE KEY `transaction_id_uindex` (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=latin1
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2016-01-23 18:58:46
